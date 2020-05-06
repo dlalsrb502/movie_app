4a78201f341aa9b715e27cdef9f2692c2215d264
dlalsrb502 <dlalsrb502@naver.com>
2020-05-07 07:12:23 +0900

    Updates

---
+++ precache-manifest.06d8dbcf59d2f6b10be5e47b84aaf210.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20119a0b2148bb522114084a459eed3",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "cb8645ca455d7141f9d1",
+    "url": "/movie_app/static/css/main.45a158db.chunk.css"
+  },
+  {
+    "revision": "9eaa36a40bfdb7bfe27e",
+    "url": "/movie_app/static/js/2.411a3f45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.411a3f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb8645ca455d7141f9d1",
+    "url": "/movie_app/static/js/main.aa88e91c.chunk.js"
+  },
+  {
+    "revision": "4dcb005dec2f66fe5231",
+    "url": "/movie_app/static/js/runtime-main.615d8c0e.js"
+  }
+]);
